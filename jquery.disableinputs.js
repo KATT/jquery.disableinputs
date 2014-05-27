f846445decd9737befcfd3576864876d4aaeef50
Alexander Johansson <ajax@ustwo.co.uk>
2014-05-27 14:05:27 +0100

    jshint

--- jquery.disableinputs.js
+++ jquery.disableinputs.js
@@ -1,16 +1,16 @@
 ;(function($) {
-  var pluginName = 'disableInputs';
-  var beforeStateKey = pluginName + '-before';
+  var pluginName = "disableInputs";
+  var beforeStateKey = pluginName + "-before";
 
   $.fn[pluginName] = function(disable) {
-    disable = (typeof disable == 'undefined' ? true : disable);
+    disable = (typeof disable === "undefined" ? true : disable);
 
     this.each(function() {
       var $form = $(this);
-      var $inputs = $form.find('textarea,input,select,button');
+      var $inputs = $form.find("textarea,input,select,button");
 
       if ($form.data(pluginName) === disable) {
-        // console.log('no change on disableInputs');
+        // console.log("no change on disableInputs");
         return;
       }
       $inputs.each(function(index, input) {
@@ -18,13 +18,13 @@
         var beforeState;
         if (disable) {
           // save state
-          beforeState = $input.prop('disabled');
+          beforeState = $input.prop("disabled");
           $input.data(beforeStateKey, beforeState);
-          $input.prop('disabled', 'disabled');
+          $input.prop("disabled", "disabled");
         } else {
           // retrieve state
           beforeState = $input.data(beforeStateKey);
-          $input.prop('disabled', beforeState);
+          $input.prop("disabled", beforeState);
         }
       });
 
